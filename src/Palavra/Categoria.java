84331fdd9e191740100bb783f44b1ee285255091
Elio Moratori <moratori.elio@gmail.com>
2024-11-10 16:49:42 -0300

    feat: importando palavras

---
+++ src/Palavra/Categoria.java
@@ -0,0 +1,13 @@
+package Palavra;
+
+import java.util.ArrayList;
+
+public class Categoria {
+    ArrayList<String> meiosDeTransporte;
+    ArrayList<String> animais;
+    ArrayList<String> comidas;
+    ArrayList<String> sentimentos;
+    ArrayList<String> paises;
+    ArrayList<String> objetos;
+
+}
---
+++ src/Util/TrabalhandoComLista.java
@@ -0,0 +1,24 @@
+package Util;
+
+import Palavra.Palavra;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class TrabalhandoComLista {
+
+   private String json = """
+        {
+            "meiosDeTransporte": ["carro", "avião", "trem", "ônibus", "barco", "bicicleta", "motocicleta", "patinete", "táxi", "metrô", "van", "caminhão", "helicóptero", "dirigível", "canoa", "escuna", "veleiro", "lancha", "submarino", "foguete"],
+            "animais": ["cachorro", "gato", "elefante", "girafa", "leão", "tigre", "macaco", "cobra", "pássaro", "peixe", "baleia", "golfinho", "cavalo", "vaca", "ovelha", "porco", "galinha", "pato", "rato", "coelho", "hiena", "panda", "camelo", "canguru", "morcego", "coruja", "águia", "formiga", "abelha"],
+            "comidas": ["pizza", "lasanha", "hambúrguer", "sushi", "macarrão", "arroz", "feijão", "frango", "carne", "peixe", "salada", "sopa", "bolo", "torta", "biscoito", "chocolate", "sorvete", "fruta", "leite", "suco", "café", "chá", "vinho", "cerveja", "sanduíche", "omelete", "panqueca", "pastel"],
+            "sentimentos": ["alegria", "tristeza", "amor", "ódio", "raiva", "medo", "surpresa", "calma", "ansiedade", "felicidade", "inveja", "ciúme", "orgulho", "vergonha", "solidão", "esperança", "desesperança", "satisfação", "frustração", "saudade"],
+            "paises": ["Brasil", "Estados Unidos", "Japão", "China", "Índia", "Rússia", "Alemanha", "França", "Reino Unido", "Itália", "Canadá", "Austrália", "Espanha", "Portugal", "México", "Argentina", "Chile", "Colômbia", "Peru", "Venezuela"],
+            "objetos": ["cadeira", "mesa", "lápis", "caneta", "livro", "caderno", "computador", "celular", "relógio", "chave", "porta", "janela", "bola", "carro", "boneca", "carrinho", "brinquedo", "faca", "garfo", "colher", "copo", "prato", "xícara", "panela", "geladeira", "fogão", "cama", "almofada"]
+        }
+        
+        """;
+
+
+}
+
+
